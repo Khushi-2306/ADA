9140e784044de49d1a71fe504e60de1c0ea25831
Khushi-2306 <khushishetty2307@gmail.com>
2026-01-23 10:18:49 +0530

    Add files via upload

---
+++ insertion sort.py
@@ -0,0 +1,17 @@
+def insertion_sort(arr):
+    n = len(arr)
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+    return arr
+
+arr = list(map(int, input("Enter elements separated by space: ").split()))
+sorted_arr = insertion_sort(arr)
+print("Sorted array:", sorted_arr)
